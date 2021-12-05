ad2173b1e3342fe36cd889a60cabca0b55093728
maxim.saraev <maxim.saraev@dataart.com>
2021-12-05 19:05:23 +0300

    fix all unit tests

--- src/app/core/error-handle.interceptor.spec.ts
+++ src/app/core/error-handle.interceptor.spec.ts
@@ -1,13 +1,22 @@
 import { TestBed } from '@angular/core/testing';
+import { Router } from '@angular/router';
 
 import { ErrorHandleInterceptor } from './error-handle.interceptor';
 
 describe('ErrorHandleInterceptor', () => {
-  beforeEach(() => TestBed.configureTestingModule({
-    providers: [
-      ErrorHandleInterceptor
+  let routerSpy: jasmine.SpyObj<Router>;
+
+  beforeEach(() => {
+    routerSpy = jasmine.createSpyObj('Router', ['navigate']);
+
+    TestBed.configureTestingModule({
+      providers: [
+        ErrorHandleInterceptor,
+        { provide: Router, useValue: routerSpy }
       ]
-  }));
+    });
+
+  });
 
   it('should be created', () => {
     const interceptor: ErrorHandleInterceptor = TestBed.inject(ErrorHandleInterceptor);
